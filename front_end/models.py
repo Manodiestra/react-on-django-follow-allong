97743aa7538585cae3d43c5e18aaecceb77a8ba8
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2024-03-31 09:58:07 -0600

    Working on server side auth. Needs a custom user model.

--- front_end/models.py
+++ front_end/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from django.db import models
 
 class JournalEntry(models.Model):
-    user_id = models.CharField(max_length=200)
+    user_id = models.CharField(max_length=255)
     title = models.CharField(max_length=200)
     content = models.TextField()
     dateTime = models.DateTimeField()
 
-# Always make migrations after updating this file
+# Remember to make migrations after changing this file!
 #
 # python manage.py makemigrations
 # python manage.py migrate
--- front_end/views.py
+++ front_end/views.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from django.http import Http404, HttpResponseServerError
 from django.utils._os import safe_join
 from django.views.static import serve as static_serve
-from rest_framework.response import Response
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework import viewsets, status
+from rest_framework.response import Response
 from django.utils.timezone import now
 
 from .models import JournalEntry
@@ -34,30 +34,39 @@ class TokenPresent(BasePermission):
     def has_permission(self, request, view):
         return 'Authorization' in request.headers
 
+
 class JournalEntryViewSet(viewsets.ModelViewSet):
     serializer_class = JournalEntrySerializer
-    # Use custom permission class or remove it entirely if not needed
-    permission_classes = [TokenPresent]
+    permission_classes = [IsAuthenticated]  # Apply the permission class to all actions
 
     def get_queryset(self):
-        # Get the user ID from a custom header or part of the request
-        user_id = self.request.headers.get('X-User-ID')
+        # Filter the queryset by the authenticated user's Cognito User ID
+        user_id = self.request.user.username  # Cognito User ID stored in `username`
         return JournalEntry.objects.filter(user_id=user_id)
 
     def perform_create(self, serializer):
-        # Get the user ID from the request data and save it with the journal entry
-        user_id = self.request.data.get('user_id')
-        serializer.save(user_id=user_id)
+        # Automatically save the user_id when creating a new Journal Entry
+        serializer.save(user_id=self.request.user.username)
 
-    # No need to override `create` and `update` methods if they don't have specific logic other than calling `perform_create` and `perform_update`
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-
-        # Ensure the instance user_id matches the one in the request data or headers
-        request_user_id = request.data.get('user_id') or request.headers.get('X-User-ID')
-        if instance.user_id != request_user_id:
+        if instance.user_id != request.user.username:
             return Response({"detail": "You do not have permission to update this entry."}, status=status.HTTP_403_FORBIDDEN)
 
-        return super(JournalEntryViewSet, self).update(request, *args, **kwargs)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
